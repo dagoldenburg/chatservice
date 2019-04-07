ea41f8cb3e0558ecf8ba0dc934cf3e58f5e89b0f
Dag <dagoldenburg@gmail.com>
2019-04-07 13:02:45 +0200

    ghj

--- src/main/java/com/rofl/chatservice/ChatController.java
+++ src/main/java/com/rofl/chatservice/ChatController.java
@@ -1,29 +1,41 @@
 package com.rofl.chatservice;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping("/v1")
 public class ChatController {
-/*
+
+    final FluxProcessor processor;
+    final FluxSink sink;
+    AtomicLong counter;
+
 
     private ArrayList<ChatMessage> messages = new ArrayList<>();
 
-    @PostMapping("/postMessage")
-    private Mono<Void> postMessage(@Valid @RequestBody ChatMessage message) {
-        messages.add(message);
-        return Mono.empty();
+    public ChatController(FluxProcessor processor, FluxSink sink) {
+        this.processor = processor;
+        this.sink = sink;
+        this.counter = new AtomicLong();
     }
 
-    @GetMapping
-    private Flux<ChatMessage> subscribeToMessageList() {
-        return null;
+    @PostMapping("/postMessage")
+    private void postMessage(@Valid @RequestBody ChatMessage message) { ;
+        messages.add(message);
+        sink.next(message);
     }
-*/
+
+    @RequestMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ServerSentEvent> subscriptionList() {
+        return processor
+                .map(e -> ServerSentEvent.builder(e).build());
+    }
+
 
 }
--- src/main/java/com/rofl/chatservice/ChatserviceApplication.java
+++ src/main/java/com/rofl/chatservice/ChatserviceApplication.java
@@ -2,15 +2,26 @@ package com.rofl.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.DirectProcessor;
+import reactor.core.publisher.FluxProcessor;
+import reactor.core.publisher.FluxSink;
 
 @SpringBootApplication
 public class ChatserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatserviceApplication.class, args);
+	}
 
-		//GreetingWebClient g = new GreetingWebClient();
-		//System.out.println(g.getResult());
+	@Bean
+	public FluxProcessor fluxProcessor() {
+		return DirectProcessor.create().serialize();
+	}
+
+	@Bean
+	public FluxSink fluxSink(FluxProcessor fluxProcessor) {
+		return fluxProcessor.sink();
 	}
 
 }
